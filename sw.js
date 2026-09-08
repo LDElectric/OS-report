a3aa46809543ad5d89d47c878d34e440f7d550f8
LDElectric <leonamdias3@gmail.com>
2026-09-08 14:09:49 -0300

    Adiciona botao unico Gerar PDF com compartilhamento nativo (WhatsApp/e-mail/salvar) e biblioteca offline

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'os-fotos-v3';
+const CACHE_NAME = 'os-fotos-v4';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png'
+  './icon.png',
+  './lib/html2pdf.bundle.min.js'
 ];
 
 // Instala: pré-cacheia os arquivos essenciais
